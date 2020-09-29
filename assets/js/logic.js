6f38006fce580469fc0e04caeac7c59be51c90b2
MadelineC <madeline.cowell@colorado.edu>
2020-09-29 08:23:04 -0600

    first commit

---
+++ assets/js/logic.js
@@ -0,0 +1,41 @@
+// variables to keep track of quiz state
+
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+
+// variables to ref DOM elements
+
+var questionsEl = document.getElementById("questions");
+
+// var timerEl = 
+// var choicesEl = 
+// var submitBtn = 
+// var startBtn = 
+// var initialsEl = 
+// var feedbackEl = 
+
+function startQuiz () {
+
+}
+
+function clockTick () {
+
+}
+
+function getQuestion () {
+
+}
+
+function questionClick () {
+
+}
+
+function quizEnd () {
+
+}
+
+// user clicks button to start quiz
+
+startBtn.onclick = startQuiz;
